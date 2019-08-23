686359a2fa1ae9778185b3e8b714f1aca5ff5d3c
Enor Anidi <enoran@Overflow.local>
2019-08-23 15:50:01 +0100

    Updates to prototype

--- app/routes.js
+++ app/routes.js
@@ -3,4 +3,15 @@ const router = express.Router()
 
 // Add your routes here - above the module.exports line
 
+router.post('/sprint-future/exporter/apply/location-uk', function (req, res) {
+
+    let locationUK = req.session.data['location']
+
+    if (locationUK == 'true') {
+        res.redirect('/sprint-future/exporter/apply/location-uk')
+    } else {
+        res.redirect('/sprint-future/exporter/apply/location-outside-uk')
+    }
+})
+
 module.exports = router
