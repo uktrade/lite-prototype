80d2b732b8a78a56a39be881e17618ab73879132
Enor Anidi <enoran@Overflow.local>
2019-08-28 22:02:49 +0100

    Updates to prototype

--- app/routes.js
+++ app/routes.js
@@ -18,31 +18,31 @@ router.post('/sprint-future/exporter/apply/tbc/location-uk', function (req, res)
 router.get('/sprint-future/exporter/apply/list', (req, res, next) => {
 
     if (!req.session.sectionStatus){
-      req.session.sectionStatus = {
-        check: undefined,
-        tbc: undefined,
-        items: undefined,
-      }
+    	req.session.sectionStatus = {
+        	check: undefined,
+        	tbc: undefined,
+        	items: undefined,
+      	}
     }
   
     if (req.query.check) {
-        req.session.sectionStatus.check = req.query.check
-      };
+    	req.session.sectionStatus.check = req.query.check
+    };
     if (req.query.tbc) {
-      req.session.sectionStatus.tbc = req.query.tbc
+    	req.session.sectionStatus.tbc = req.query.tbc
     };
     if (req.query.items) {
-      req.session.sectionStatus.items = req.query.items
+    	req.session.sectionStatus.items = req.query.items
     };
   
     res.render('sprint-future/exporter/apply/list.html', {sectionStatus: req.session.sectionStatus});
   });
   
-  // Clear data on the 'application cancelled' screen
-  router.get('/*/clear-v9', function (req, res) {
-    req.session.destroy()
+// Clear data on the 'application cancelled' screen
+router.get('/*/clear-apply', function (req, res) {
+	req.session.destroy()
     res.render('sprint-future/exporter/apply/application-cancelled')
-  })
+})
 
 
 
