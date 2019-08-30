693bbd171153800401d0ce5257485d1db8256af8
Enor Anidi <enoran@Overflow.local>
2019-08-30 23:43:19 +0100

    Updates to prototype

--- app/routes.js
+++ app/routes.js
@@ -15,6 +15,29 @@ router.post('/sprint-future/exporter/apply/tbc/location-uk', function (req, res)
     }
 })
 
+// Remove items routing
+router.post('/sprint-future/exporter/apply/items/index', function (req, res) {
+
+    let itemAdd = req.session.data['item-add']
+
+    if (itemAdd === "Yes") {
+        res.redirect('/sprint-future/exporter/apply/items/index')
+    } else {
+        res.redirect('/sprint-future/exporter/apply/task-list?items=completed')
+    }
+})
+
+router.post('/sprint-future/exporter/apply/items/item-removed', function (req, res) {
+
+    let itemRemove = req.session.data['item-remove']
+
+    if (itemRemove === "Yes") {
+        res.redirect('/sprint-future/exporter/apply/items/item-removed')
+    } else {
+        res.redirect('/sprint-future/exporter/apply/items/item-added')
+    }
+})
+
 // Task list 
 router.get('/sprint-future/exporter/apply/task-list', (req, res, next) => {
 
