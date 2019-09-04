3f51afb586659fe562992a0676896bbc2393c83d
Enor Anidi <enoran@Overflow.local>
2019-09-04 16:04:23 +0100

    Updates to prototype

--- app/routes.js
+++ app/routes.js
@@ -4,136 +4,136 @@ const router = express.Router()
 // Add your routes here - above the module.exports line
 
 // UK/Non-UK location
-router.post('/sprint-future/exporter/apply/products/location-uk', function (req, res) {
+router.post('/exporter/apply/products/location-uk', function (req, res) {
 
     let locationUK = req.session.data['location']
 
     if (locationUK === "In the UK") {
-        res.redirect('/sprint-future/exporter/apply/products/location-uk')
+        res.redirect('/exporter/apply/products/location-uk')
     } else {
-        res.redirect('/sprint-future/exporter/apply/products/location-outside-uk')
+        res.redirect('/exporter/apply/products/location-outside-uk')
     }
 })
 
 // UK/Non-UK location [Product 1]
-router.post('/sprint-future/exporter/apply/products/location-uk-1', function (req, res) {
+router.post('/exporter/apply/products/location-uk-1', function (req, res) {
 
     let locationUK1 = req.session.data['location-1']
 
     if (locationUK1 === "In the UK") {
-        res.redirect('/sprint-future/exporter/apply/products/location-uk-1')
+        res.redirect('/exporter/apply/products/location-uk-1')
     } else {
-        res.redirect('/sprint-future/exporter/apply/products/location-outside-uk-1')
+        res.redirect('/exporter/apply/products/location-outside-uk-1')
     }
 })
 
 // UK/Non-UK location [Product 2]
-router.post('/sprint-future/exporter/apply/products/location-uk-2', function (req, res) {
+router.post('/exporter/apply/products/location-uk-2', function (req, res) {
 
     let locationUK2 = req.session.data['location-2']
 
     if (locationUK2 === "In the UK") {
-        res.redirect('/sprint-future/exporter/apply/products/location-uk-2')
+        res.redirect('/exporter/apply/products/location-uk-2')
     } else {
-        res.redirect('/sprint-future/exporter/apply/products/location-outside-uk-2')
+        res.redirect('/exporter/apply/products/location-outside-uk-2')
     }
 })
 
 // Remove products 
-router.post('/sprint-future/exporter/apply/products/index', function (req, res) {
+router.post('/exporter/apply/products/index', function (req, res) {
 
     let itemAdd = req.session.data['item-add']
 
     if (itemAdd === "Yes") {
-        res.redirect('/sprint-future/exporter/apply/products/index')
+        res.redirect('/exporter/apply/products/index')
     } else {
-        res.redirect('/sprint-future/exporter/apply/task-list?products=completed')
+        res.redirect('/exporter/apply/task-list?products=completed')
     }
 })
 
-router.post('/sprint-future/exporter/apply/products/remove-product', function (req, res) {
+router.post('/exporter/apply/products/remove-product', function (req, res) {
 
     let itemRemove = req.session.data['remove-product']
 
     if (itemRemove === "Yes") {
-        res.redirect('/sprint-future/exporter/apply/products/remove-product')
+        res.redirect('/exporter/apply/products/remove-product')
     } else {
-        res.redirect('/sprint-future/exporter/apply/products/product-added')
+        res.redirect('/exporter/apply/products/product-added')
     }
 })
 
 // Remove products [Product 1]
-router.post('/sprint-future/exporter/apply/products/index-2', function (req, res) {
+router.post('/exporter/apply/products/index-2', function (req, res) {
 
     let itemAdd1 = req.session.data['item-add-1']
 
     if (itemAdd1 === "Yes") {
-        res.redirect('/sprint-future/exporter/apply/products/index-2')
+        res.redirect('/exporter/apply/products/index-2')
     } else {
-        res.redirect('/sprint-future/exporter/apply/task-list?products=completed')
+        res.redirect('/exporter/apply/task-list?products=completed')
     }
 })
 
-router.post('/sprint-future/exporter/apply/products/product-removed', function (req, res) {
+router.post('/exporter/apply/products/product-removed', function (req, res) {
 
     let itemRemove1 = req.session.data['remove-product']
 
     if (itemRemove1 === "Yes") {
-        res.redirect('/sprint-future/exporter/apply/products/product-removed')
+        res.redirect('/exporter/apply/products/product-removed')
     } else {
-        res.redirect('/sprint-future/exporter/apply/products/product-added')
+        res.redirect('/exporter/apply/products/product-added')
     }
 })
 
 // Remove products [Product 2]
-router.post('/sprint-future/exporter/apply/products/index', function (req, res) {
+router.post('/exporter/apply/products/index', function (req, res) {
 
     let itemAdd2 = req.session.data['item-add-2']
 
     if (itemAdd2 === "Yes") {
-        res.redirect('/sprint-future/exporter/apply/products/index')
+        res.redirect('/exporter/apply/products/index')
     } else {
-        res.redirect('/sprint-future/exporter/apply/task-list?products=completed')
+        res.redirect('/exporter/apply/task-list?products=completed')
     }
 })
 
-router.post('/sprint-future/exporter/apply/products/product-removed', function (req, res) {
+router.post('/exporter/apply/products/product-removed', function (req, res) {
 
     let itemRemove2 = req.session.data['remove-product']
 
     if (itemRemove2 === "Yes") {
-        res.redirect('/sprint-future/exporter/apply/products/product-removed')
+        res.redirect('/exporter/apply/products/product-removed')
     } else {
-        res.redirect('/sprint-future/exporter/apply/products/product-added')
+        res.redirect('/exporter/apply/products/product-added')
     }
 })
 
 // User type
-router.post('/sprint-future/exporter/apply/people/organisation', function (req, res) {
+router.post('/exporter/apply/people/organisation', function (req, res) {
 
     let userType = req.session.data['user-type']
 
     if (userType === "Organisation") {
-        res.redirect('/sprint-future/exporter/apply/people/organisation')
+        res.redirect('/exporter/apply/people/organisation')
     } else {
-        res.redirect('/sprint-future/exporter/apply/people/individual')
+        res.redirect('/exporter/apply/people/individual')
     }
 })
 
 // End user undertaking
-router.post('/sprint-future/exporter/apply/end-user/upload-document', function (req, res) {
+router.post('/exporter/apply/end-user/upload-document', function (req, res) {
 
     let euuType = req.session.data['euu-document']
 
     if (euuType === "Uploaded") {
-        res.redirect('/sprint-future/exporter/apply/end-user/upload-document')
+        res.redirect('/exporter/apply/end-user/upload-document')
     } else {
-        res.redirect('/sprint-future/exporter/apply/end-user/cya')
+        res.redirect('/exporter/apply/end-user/cya')
     }
 })
 
 // Task list 
-router.get('/sprint-future/exporter/apply/task-list', (req, res, next) => {
+router.get('/exporter/apply/task-list', (req, res, next) => {
 
     if (!req.session.sectionStatus){
     	req.session.sectionStatus = {
@@ -165,13 +165,13 @@ router.get('/sprint-future/exporter/apply/task-list', (req, res, next) => {
     	req.session.sectionStatus.information = req.query.information
     };
   
-    res.render('sprint-future/exporter/apply/task-list.html', {sectionStatus: req.session.sectionStatus});
+    res.render('exporter/apply/task-list.html', {sectionStatus: req.session.sectionStatus});
 });
   
 // Clear data on the 'Application cancelled' page
 router.get('/*/application-cancelled', function (req, res) {
 	req.session.destroy()
-    res.render('sprint-future/exporter/apply/confirm-cancel')
+    res.render('exporter/apply/confirm-cancel')
 })
 
 module.exports = router
