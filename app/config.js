20f3ef8d7ceabe12c4861db662604b233ec522da
Enor Anidi-Ryz <hello@enoranidi.com>
2019-12-06 14:26:51 +0000

    text updates

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'LITE Prototypes',
+  serviceName: 'LITE prototypes',
 
   // Default port that prototype runs on
   port: '3000',
